26083f65375aa14e3f62c4afce9e722526517d10
israel <israel61167@gmail.com>
2019-05-26 11:19:11 +0300

    class chenges

--- src/App.js
+++ src/App.js
@@ -1,62 +1,27 @@
-import React, { Component } from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 64e629a6652fcb501baacc7fb2151b7a58e57e01
+import React, { Component } from "react"
 import HomePage from './HomePage/HomePage.js'
 import NewClass from "./NewClass/NewClass.js";
 import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
 import LogIn from "./Login/LogIn.js";
-
-
 import EditProfile from "./Login/EditProfile.js"
-<<<<<<< HEAD
-=======
-import ManagePage from "./ManagePage/ManagePage";
-import NavBar from "./NavBar/NavBar";
-import NewClass from "./NewClass/NewClass";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import AllCaterogy from './HomePage/AllCategory.js';
-import Classs from './Class/Class' ;
->>>>>>> israel
-=======
 import Category from "./Category/Category.js";
->>>>>>> 64e629a6652fcb501baacc7fb2151b7a58e57e01
+import Classs from './Class/Class.js'
 
 class App extends Component {
   
   render() {
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 64e629a6652fcb501baacc7fb2151b7a58e57e01
-      <Router>
+       <Router>
         <Switch>
         <Route path="/" exact component={HomePage}/>
         <Route path="/NewClass" exact component={NewClass}/>
         <Route path="/login" exact component={LogIn}/>
         <Route path="/EditProfile" exact component={EditProfile}/>
         <Route path="/Category/:name" exact render={({match}) => {return <Category name={match.params.name}/>;}}/>
-        <Route path="/Category/:nameC/Class/:nameClass" exact render={({match}) => {return <Category catname={match.params.nameC} classname={match.params.nameClass}/>;}}/>
+        <Route path="/Category/:nameC/Class/:nameClass" exact render={({match}) => {return <Classs catname={match.params.nameC} classname={match.params.nameClass}/>;}}/>
         </Switch>
       </Router>
-=======
-        <Router>
-          <Route path="/Class" exact component={Classs} />
-          <Route path="/NewClass" exact component={NewClass} />
-          <Route path="/" render={() => { return <NavBar />;}}/>
-          <Route path="/" exact render={() => { return (
-          <div>
-          <br></br>
-          <AllCaterogy/>
-          </div>
-          );}}/>
-          
-        </Router>
->>>>>>> israel
+
     );
   }
 }
--- src/Class/Class.js
+++ src/Class/Class.js
@@ -1,38 +1,10 @@
 import React from "react"
 import firebase from "../Firebase/FireBase.js";
-import "@ionic/core/css/core.css";
-import "@ionic/core/css/ionic.bundle.css";
 import "./Class.css"
 import Button from '@material-ui/core/Button';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, ListGroup, ListGroupItem} from 'reactstrap';
 
-
-function Join() 
-{
-  return (
-    <Button variant="contained" color="primary">
-      Join
-    </Button>)
-}
-
-function ClassCard()
-{
-    return(
-        <Card style={{ width: '18rem' }}>
-            <CardImg variant="top" src="https://firebasestorage.googleapis.com/v0/b/mayesh-bd07f.appspot.com/o/imgs%2Fyoga.jpg?alt=media&token=1c744d8c-f29b-40b1-82aa-a9efc1ba97dc" />
-            <CardBody>
-                <CardTitle>Card Title</CardTitle>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
-                <Button variant="contained" color="primary">Join</Button>
-            </CardBody>
-      </Card>
-    )
-}
 
 
 class Classs extends React.Component
@@ -40,28 +12,62 @@ class Classs extends React.Component
     constructor(props)
     {
         super(props);
-        
-        this.state = {isSignedIn: false, newUser:true, loading : true,
-            thisClass:{name:"",location:"",teacher:"",phone:"",img:"",hour:"",description:"", numOfCurrPartici:""} };
+        this.state = {category: props.catname, course: props.classname, isSignedIn: false, newUser:true, loading : true,
+            thisClass:{name:"",location:"",teacher:"",phone:"",img:"",hour:"",description:"", numOfCurrPart: 0} };
+    }
+
+    componentDidMount()
+    {
+        let ref = firebase.database().ref('/CategoryList/' + this.state.category + '/classList/' + this.state.course);
+        ref.once('value', snapshot => {
+            this.setState({thisClass:{
+                                    name:snapshot.val().name,
+                                    description:snapshot.val().description,
+                                    img:snapshot.val().imgUrl,
+                                    location: snapshot.val().location, 
+                                    numOfCurrPartici: snapshot.val().numOfCurrPart
+                                }})})
     }
     
+    numOfPart() //returns the number of participants in this class.. if there is not, returns that there is not yet
+    {
+        let howManyPart = ""
+        if(this.state.thisClass.numOfCurrPartici <= 0)
+        {
+            howManyPart = "אין עדיין משתתפים בקורס זה";
+        }
+        else
+        {
+            howManyPart = "משתתפים כרגע" + this.state.thisClass.numOfCurrPartici + "אנשים";
+        }
+        return(
+             howManyPart
+        )
+    }
+
+    whenClicked()
+    {
+        //sighin
+        //class is not full
+        //numOfCurrPart++
+        //join sucessfully
+        this.state.thisClass.numOfCurrPart = 1;
+        alert(this.state.thisClass.numOfCurrPartici)
+        return(
+            "clicked"
+        )
+    }
+
     render()
     {
-        let category = "Art"
-        let kourse = "יוגה עם חיים"
-        let title = "before"
-        let ref = firebase.database().ref('/CategoryList/' + category + '/classList/' + kourse);
-        ref.once('value', snapshot => {
-            this.setState({thisClass:{name:snapshot.val().name,
-                                     description:snapshot.val().description,
-                                     img:snapshot.val().image,
-                                     location: snapshot.val().location, 
-                                     numOfCurrPartici: snapshot.val().numOfCurrPartici }})})
         return(
             <div  className = "mainDiv">
                 <div className = "leftSide">
-                    <CardImg variant="top" src={this.state.thisClass.img} />
-                    <Button className = "button" variant="contained" color="primary">Join</Button>
+                    <CardImg className = "classImg" variant="top" src={this.state.thisClass.img} />
+                    <div>
+                        <Button className = "button1" variant="contained" color="primary"  onClick = {()=>this.whenClicked()}>Join</Button>
+                        <Button className = "button2" variant="contained" color="primary"  onClick = {()=>this.whenClicked()}>back</Button>
+                    </div>
                 </div>
                 <div className = "rightSide">
                     <Card style={{ width: '30rem' }}>
@@ -70,7 +76,7 @@ class Classs extends React.Component
                             <ListGroup className="list-group-flush">
                                 <p>{this.state.thisClass.description}</p>
                                 <ListGroupItem className = "location"> {this.state.thisClass.location}</ListGroupItem>
-                                <ListGroupItem className = "location">משתתפים כרגע {this.state.thisClass.numOfCurrPartici} אנשים  </ListGroupItem>
+                                <ListGroupItem className = "numOfCurrPartici">{this.numOfPart()}</ListGroupItem>
                             </ListGroup>
                         </CardBody>
                     </Card>
